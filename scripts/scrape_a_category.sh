5c40312fa74d24634e539036623d99b79c773158
Toan Phan <toanphan.cs@gmail.com>
2022-09-06 15:59:12 +0200

    chore: add bash script to scrape a category

---
+++ scripts/scrape_a_category.sh
@@ -0,0 +1,3 @@
+echo "Making POST request..."
+echo ""
+curl -X POST "https://test-crawlee-kq7q53zcma-ew.a.run.app/exploreCategory"  -H "Content-Type: application/json"  -d '{"url": "https://www.trademax.se/utem%C3%B6bler/utestolar-tr%C3%A4dg%C3%A5rdsstolar/h%C3%A4ngstol-utomhus"}' -i
