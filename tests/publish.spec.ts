e79e1e974f6027cb882e42f0acc824596e793ca7
Toan Phan <toanphan.cs@gmail.com>
2022-09-19 17:35:54 +0200

    fix: fix publish.spec test

--- tests/publish.spec.ts
+++ tests/publish.spec.ts
@@ -3,15 +3,13 @@ import {prepareForBigQuery} from "../src/publishing";
 describe("Conversion for bigquery", () => {
     test("Camel case is correctly converted to snake case", () => {
         const dummyObject = {
-            simpleWord: "value",
-            repeatingStartSymbol: "value",
-            abcAbcAbc: "value"
+            fetchedAt: "value",
+            categoryTree: "value",
         }
 
         const convertedObject = prepareForBigQuery([dummyObject])[0]
 
-        expect(convertedObject).toHaveProperty("simple_word")
-        expect(convertedObject).toHaveProperty("repeating_start_symbol")
-        expect(convertedObject).toHaveProperty("abc_abc_abc")
+        expect(convertedObject).toHaveProperty("fetched_at")
+        expect(convertedObject).toHaveProperty("category_tree")
     })
 })
