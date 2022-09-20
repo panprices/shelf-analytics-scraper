0fc60c54dce7bcd253b1f2ed108500f6086ec595
Toan Phan <toanphan.cs@gmail.com>
2022-09-20 17:20:30 +0200

    remove unintended

--- scripts/scrape_a_category_local.sh
+++
@@ -1,3 +0,0 @@
-echo "Making POST request..."
-echo ""
-curl -X POST "localhost:8080/exploreCategory"  -H "Content-Type: application/json"  -d '{"url": "https://www.venturedesign.se/utemobler/dynor"}' -i
