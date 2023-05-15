9a8f1f809a91a06accf275bfb29ed20336de9fdd
Toan Phan <toanphan.dev@gmail.com>
2023-05-15 18:33:07 +0200

    fix: homeroom size variants

---
+++ tests-dev/homeroom-dev.spec.ts
@@ -0,0 +1,73 @@
+import { exploreCategory, scrapeDetails } from "../src/service";
+
+jest.setTimeout(300000);
+
+function dummyRequest(targetUrl: string) {
+  return {
+    url: targetUrl,
+    userData: {
+      jobId: "job_test_1",
+      url: targetUrl,
+      popularityIndex: 1,
+      label: "DETAIL",
+    },
+  };
+}
+
+test("Category page", async () => {
+  const targetUrl = "https://www.homeroom.se/utemobler-tradgard/mobelskydd";
+  const result = await exploreCategory(targetUrl, "job_test_1");
+
+  expect(result).toHaveLength(46);
+});
+
+test("Product page with colour variants", async () => {
+  const targetUrl =
+    "https://www.homeroom.se/venture-home/matgrupp-tempe-med-2st-matstolar-polar/1722582-01";
+  const result = await scrapeDetails([dummyRequest(targetUrl)]);
+
+  expect(result).toHaveLength(2);
+  expect(result.map((p) => p.images.length)).toEqual([6, 8]);
+  expect(result.map((p) => p.isDiscounted)).toEqual([false, false]);
+  expect(result.map((p) => p.price)).toEqual([369900, undefined]);
+});
+
+test("Poster with size variants", async () => {
+  const targetUrl =
+    "https://www.homeroom.se/venture-home/poster-circles/1703384-01";
+  const result = await scrapeDetails([dummyRequest(targetUrl)]);
+
+  expect(result).toHaveLength(4);
+  expect(result.map((p) => p.images.length)).toEqual([2, 2, 2, 2]);
+  expect(result.map((p) => p.sku)).toEqual([
+    "1703384-01-345",
+    "1703384-01-118",
+    "1703384-01-136",
+    "1703384-01-48",
+  ]);
+  expect(result.map((p) => p.isDiscounted)).toEqual([
+    false,
+    false,
+    false,
+    false,
+  ]);
+  expect(result.map((p) => p.price)).toEqual([20300, 22100, 32900, 35400]);
+});
+
+test("Product page with both colour and size variants", async () => {
+  const targetUrl =
+    "https://www.homeroom.se/venture-home/ullmatta-loump/1675496-01";
+  const result = await scrapeDetails([dummyRequest(targetUrl)]);
+
+  expect(result).toHaveLength(4);
+});
+
+test("Only 1 image", async () => {
+  const targetUrl =
+    "https://www.homeroom.se/venture-home/mobelskydd-130-210-160-gra/1560334-01";
+
+  const result = await scrapeDetails([dummyRequest(targetUrl)]);
+  expect(result).toHaveLength(1);
+  expect(result.map((p) => p.images.length)).toEqual([1]);
+  expect(result.map((p) => p.sku)).toEqual(["1560334-01-0"]);
+});
